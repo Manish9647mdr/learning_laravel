59417e54274cec726bee1874e4003c960307fb72
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-02-06 15:30:57 +0900

    Learn Eloquent(Object Relation Mapping)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,43 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40000'
-            ],
-        ];
-    }
+    // get the data from the database table
+    protected $table = 'job_listings';
 
-
-    // find the job by its id
-    public static function find(int $id): array
-    {
-        // search for a job with the given id
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        // if no job is found , abort with a 404 error
-        if (! $job) {
-            abort(404);
-        }
-        // return the found job details
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
