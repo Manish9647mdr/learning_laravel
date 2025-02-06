6e2d989af3ba2a2588fea54de8b74c465a537f01
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-02-06 17:36:14 +0900

    Model Factories Learning

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+    use HasFactory;
+
     // get the data from the database table
     protected $table = 'job_listings';
 
