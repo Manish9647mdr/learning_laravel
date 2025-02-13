24866f6a27cae1f4802151aceae4dd03e98f452f
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-02-13 16:45:52 +0900

    edit, update and delete a resource

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,20 @@ Route::get('/jobs', function () {
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create a new Job
 Route::get('/jobs/create', function () {
     return view('jobs.create ');
 });
 
-
+// show a job
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// store a job
 Route::post('/jobs', function () {
-    // validation...
-
     request()->validate([
         'title' => ['required', 'min:3'],
         'salary' => ['required'],
@@ -41,6 +41,43 @@ Route::post('/jobs', function () {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// edit a job
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// update a job
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize
+
+    // update the job
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    //redirect
+    return redirect('/jobs/ ' . $job->id);
+});
+
+// delete a job
+Route::delete('/jobs/{id}', function ($id) {
+    // delete a job
+    Job::findOrFail($id)->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
