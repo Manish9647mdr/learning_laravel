43a17c9b3839a5619182fe3981d09a4faa687697
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-02-14 11:59:11 +0900

    form components created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -16,13 +17,7 @@ Route::controller(JobController::class)->group(function () {
     Route::delete('/jobs/{job}',  'destroy');
 });
 
-
-// Route::get('/jobs', [JobController::class, 'index']);
-// Route::get('/jobs/create', [JobController::class, 'create']);
-// Route::get('/jobs/{job}', [JobController::class, 'show']);
-// Route::post('/jobs', [JobController::class, 'store']);
-// Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-// Route::patch('/jobs/{job}', [JobController::class, 'update']);
-// Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
 
 Route::view('/contact', 'contact');
